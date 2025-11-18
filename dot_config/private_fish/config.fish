2a117764d2ef2256a7ea9a0e9269a654f69a65e0
Chris-Fr-C <christophe.fr.corsi@gmail.com>
2025-11-18 23:51:55 +0100

    update pkgs

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,13 +1,13 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
-   # Better PATH handling
-   set -gx PATH $HOME/.local/bin $PATH
+    # Better PATH handling
+    set -gx PATH $HOME/.local/bin $PATH
 
-   # Enable truecolor
-   set -gx TERM xterm-256color
+    # Enable truecolor
+    set -gx TERM xterm-256color
 
-   # Neovim as default editor
-   set -gx EDITOR nvim
+    # Neovim as default editor
+    set -gx EDITOR nvim
 
 end
