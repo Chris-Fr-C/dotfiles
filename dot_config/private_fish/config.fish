f4326a4dae32d93ac215494ebd1d5f718e23a685
Chris-Fr-C <christophe.fr.corsi@gmail.com>
2025-11-19 11:09:45 +0100

    add proper fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,13 +1,4 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
-    # Better PATH handling
-    set -gx PATH $HOME/.local/bin $PATH
-
-    # Enable truecolor
-    set -gx TERM xterm-256color
-
-    # Neovim as default editor
-    set -gx EDITOR nvim
-
 end
--- dot_config/private_fish/functions/cdchezmoi.fish
+++
@@ -1,3 +0,0 @@
-function cdchezmoi --wraps='cd ~/.local/share/chezmoi' --description 'alias cdchezmoi=cd ~/.local/share/chezmoi'
-    cd ~/.local/share/chezmoi $argv
-end
