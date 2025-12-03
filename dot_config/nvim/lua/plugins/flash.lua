2a7da57588554b7548b6e97ffbcf7973b52740ec
Chris-Fr-C <christophe.fr.corsi@gmail.com>
2025-12-03 11:07:56 +0100

    cleanup flash

--- dot_config/nvim/lua/plugins/flash.lua
+++ dot_config/nvim/lua/plugins/flash.lua
@@ -2,25 +2,7 @@ return {
     "folke/flash.nvim",
     event = "VeryLazy",
     ---@type Flash.Config
-    opts = {
-        -- Activating the defualt mode
-        modes = {
-            -- options used when flash is activated through
-            -- a regular search with `/` or `?`
-            search = {
-                -- when `true`, flash will be activated during regular search by default.
-                -- You can always toggle when searching with `require("flash").toggle()`
-                enabled = false,
-                highlight = { backdrop = false },
-                jump = { history = true, register = true, nohlsearch = true },
-                search = {
-                    -- `forward` will be automatically set to the search direction
-                    -- `mode` is always set to `search`
-                    -- `incremental` is set to `true` when `incsearch` is enabled
-                },
-            },
-        },
-    },
+    opts = {},
     keys = {
         {
             "s",
