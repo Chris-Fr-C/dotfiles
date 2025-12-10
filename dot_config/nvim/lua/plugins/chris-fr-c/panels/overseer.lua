46a3c3a81eadccd9b09802e15fa49bca6e3df061
Chris-Fr-C <christophe.fr.corsi@gmail.com>
2025-12-10 16:05:15 +0100

    shortcuts

--- dot_config/nvim/lua/plugins/chris-fr-c/panels/overseer.lua
+++ dot_config/nvim/lua/plugins/chris-fr-c/panels/overseer.lua
@@ -1,20 +1,55 @@
 -- Tasks handler.
 return {
   "stevearc/overseer.nvim",
-  opts = function()
-    -- Ensure Overseer is loaded
-    local overseer = require("overseer")
-
-    -- Keymap to open Overseer task template picker
-    vim.keymap.set("n", "<leader>ot", function()
-      overseer.run_template()
-    end, { desc = "Run Overseer Task Template" })
-
-    return {
-      templates = {
-        "builtin",
-        "task",
+  cmd = {
+    "OverseerOpen",
+    "OverseerClose",
+    "OverseerToggle",
+    "OverseerSaveBundle",
+    "OverseerLoadBundle",
+    "OverseerDeleteBundle",
+    "OverseerRunCmd",
+    "OverseerRun",
+    "OverseerInfo",
+    "OverseerBuild",
+    "OverseerQuickAction",
+    "OverseerTaskAction",
+    "OverseerClearCache",
+  },
+  opts = {
+    dap = false,
+    task_list = {
+      bindings = {
+        ["<C-h>"] = false,
+        ["<C-j>"] = false,
+        ["<C-k>"] = false,
+        ["<C-l>"] = false,
       },
-    }
-  end,
+    },
+    form = {
+      win_opts = {
+        winblend = 0,
+      },
+    },
+    confirm = {
+      win_opts = {
+        winblend = 0,
+      },
+    },
+    task_win = {
+      win_opts = {
+        winblend = 0,
+      },
+    },
+  },
+  -- stylua: ignore
+  keys = {
+    { "<leader>ow", "<cmd>OverseerToggle<cr>",      desc = "Task list" },
+    { "<leader>oo", "<cmd>OverseerRun<cr>",         desc = "Run task" },
+    { "<leader>oq", "<cmd>OverseerQuickAction<cr>", desc = "Action recent task" },
+    { "<leader>oi", "<cmd>OverseerInfo<cr>",        desc = "Overseer Info" },
+    { "<leader>ob", "<cmd>OverseerBuild<cr>",       desc = "Task builder" },
+    { "<leader>ot", "<cmd>OverseerTaskAction<cr>",  desc = "Task action" },
+    { "<leader>oc", "<cmd>OverseerClearCache<cr>",  desc = "Clear cache" },
+  },
 }
