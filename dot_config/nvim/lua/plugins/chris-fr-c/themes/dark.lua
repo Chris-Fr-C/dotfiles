01b77c2d6c112fa201fb228789efb6773e3e30e9
Chris-Fr-C <christophe.fr.corsi@gmail.com>
2025-12-12 18:04:04 +0100

    add themes and z

---
+++ dot_config/nvim/lua/plugins/chris-fr-c/themes/dark.lua
@@ -0,0 +1,7 @@
+return {
+  { "cyberdream.nvim" },
+  { "ellisonleao/gruvbox.nvim" },
+  { "projekt0n/github-nvim-theme" },
+  { "nyoom-engineering/oxocarbon.nvim" },
+  { "bluz71/vim-moonfly-colors" },
+}
