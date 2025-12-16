0b9040c7fadace48ece958fad0f39ea859e97521
Chris-Fr-C <christophe.fr.corsi@gmail.com>
2025-12-16 15:21:29 +0100

    debugger

--- dot_config/nvim/lua/plugins/chris-fr-c/code/doge.lua
+++ dot_config/nvim/lua/plugins/chris-fr-c/code/doge.lua
@@ -4,6 +4,7 @@ return {
   config = function()
     vim.cmd(":call doge#install()")
     vim.keymap.set("n", "<Leader>cg", "::DogeGenerate<CR>")
+    vim.g.vdoge_enable_mapping = false
     vim.g.doge_python_settings = {
       single_quotes = false,
       omit_redundant_param_types = false,
