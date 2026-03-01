230995b7389a10d551438862450fc9c386694b01
Chris-Fr-C <christophe.fr.corsi@gmail.com>
2026-03-01 15:14:35 +0100

    update to be linked with nix

--- generate_brew.sh
+++
@@ -1,2 +0,0 @@
-mkdir -p ~/,config/brew
-brew bundle dump --file ~/.config/brew/Brewfile --force
--- run_once_after_zsh-setup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-starship preset nerd-font-symbols -o ~/.config/starship.toml
-starship preset gruvbox-rainbow -o ~/.config/starship.toml
